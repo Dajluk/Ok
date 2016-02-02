55e9531e3ff1194779857fe79cfe3192ab730180
Dajluk <kijanka2001@wp.pl>
2016-02-02 22:05:12 +0100

    ok

---
+++ ten.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main() {
+	string name;
+	string old;
+	string color;
+	cout << "Hi!What your name?"<<endl;
+	cin>> name;
+	cout<< "Hi "<< name;
+	cout<<"! How old are you?"<<endl;
+	cin>> old;
+	cout << old<<"! Ho Ho Ho! " << " What it is your favaorite color?"<<endl;
+	cin>> color;
+	cout<< color<<"?"<<" Really? Great! "<<endl;
+	cout<< "Ok Goodbay!"<<endl;
+		return 0;
+}
